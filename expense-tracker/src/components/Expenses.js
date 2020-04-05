337909c4637742e7a5e702cd333eea9cb02994d2
Luis PM <luisp237@gmail.com>
2020-04-05 06:09:11 -0400

    add styles

--- expense-tracker/src/components/Expenses.js
+++ expense-tracker/src/components/Expenses.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import ExpensesItem from './ExpenseItem';
 
+
 const Expenses = () => {
     return (
         <div className="expense-cont">
-            <h3>Costs</h3>
+            <h3>Monthly Costs</h3>
             <ExpensesItem />
         </div>
     )
